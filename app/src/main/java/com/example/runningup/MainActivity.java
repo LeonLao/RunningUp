6f5bafedc7790f662e1128ca951211ad8e628bc5
LeonLao <leon_lao@126.com>
2016-04-05 17:10:35 +0800

    4.5commit

---
+++ app/src/main/java/com/example/runningup/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.runningup;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_main);
+        //setContentView(R.layout.maintabs);
+        setContentView(R.layout.circle_item);
+    }
+
+
+}
